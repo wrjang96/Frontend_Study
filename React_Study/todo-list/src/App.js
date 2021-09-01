58b4e0ef9ab82d0ef64137219ec13e8a586fdf7b
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-09-02 02:48:48 +0900

    add todoItem add button

--- React_Study/todo-list/src/App.js
+++ React_Study/todo-list/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false }
+    ]
+  }
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value // input 의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '', // 인풋 비우고
+      // concat 을 사용하여 배열에 추가
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 면 handleCreate 호출
+    if(e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form 
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList todos={todos}/>
+      </TodoListTemplate>
+    );
+  }
 }
-
 export default App;
