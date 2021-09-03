84db21160c58b1ba3ea1530ab1e1e27b1368cc45
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-09-03 13:40:04 +0900

    remove

--- React_Study/todo-list/src/App.js
+++ React_Study/todo-list/src/App.js
@@ -16,6 +16,56 @@ class App extends Component {
       { id: 2, text: ' 리액트 소개', checked: false }
     ]
   }
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form 
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+    );
+  }
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = { 
+      ...selected, 
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
   handleChange = (e) => {
     this.setState({
       input: e.target.value // input 의 다음 바뀔 값
@@ -47,7 +97,9 @@ class App extends Component {
     const {
       handleChange,
       handleCreate,
-      handleKeyPress
+      handleKeyPress,
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
@@ -59,7 +111,7 @@ class App extends Component {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }
--- React_Study/todo-list/src/components/TodoItemList.js
+++ React_Study/todo-list/src/components/TodoItemList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props.todos !== nextProps.todos;
+  }
+  // 업데이트에 영향을 끼치는 조건을 리턴하는 것 구현되지 않을 경우 언제나 True를 반환한다. 
+  // 이 경우에는 todos 값이 바뀔 경우에 리렌더링 해야 하니까 this.props.todos와 nextProps.todos를 비교하여 다를때만 리렌더링하게 설정하면 된다. 
+  
   render() {
     const { todos, onToggle, onRemove } = this.props;
     // todos: todo 객체들이 들어있는 배열
